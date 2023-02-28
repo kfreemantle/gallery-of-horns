d9fcc35d3ae9304979c30973d0e203d6eea676b6
Kyle Freemantle <kcfreemantle@gmail.com>
2023-02-28 13:34:37 -0800

    lab02 set up, beginning work on feature 1

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import WeeBeastie from './WeeBeastie';
 
+// Feature #1: Display Images
+// As a user, I want to view the images on the page so that I can browse the photo collection.
+// What are we going to implement?
+// Given that a user opens the application in the browser
+// When the user navigates to the home page
+// Then the photo gallery should display all of the beasts' images in the gallery.
+
+// How are we implementing it?
+// The Main component should pass props for the title, image and description to each HornedBeast component. You will find this information in the provided JSON file.
+
 class Main extends React.Component {
 
   render() {
