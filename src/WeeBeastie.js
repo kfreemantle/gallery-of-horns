07ce3dc76e398412a10277cc059d60924409ed8f
Kyle Freemantle <kcfreemantle@gmail.com>
2023-02-27 22:45:20 -0800

    working through the main.js

---
+++ src/WeeBeastie.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './WeeBeastie.css';
+
+class WeeBeastie extends React.Component {
+  render () {
+    // console.log(this.props.title);
+    return (
+      <>
+      <article>
+        <h2>{this.props.title}</h2>
+        <img src={this.props.imageURL} alt={this.props.alt} title={this.alt.title} ></img>
+        <p>{this.props.description}</p>
+
+      </article>
+      
+      </>
+    );
+  };
+};
+
+export default WeeBeastie;
